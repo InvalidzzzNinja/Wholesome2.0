ddc279c20ddfae67ca6dec405d69bdf9d039961a
InvalidzzzNinja <85476038+InvalidzzzNinja@users.noreply.github.com>
2021-09-02 10:40:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -117,4 +117,4 @@ async def purge(ctx, amount=5):
     await ctx.send(f'{amount} messages have been purged by {ctx.message.author.mention}')
 
 
-client.run("ODc5OTI3NzQ2Njg2NTA5MDg4.YSW2kw.vC9W82AA4IeLVw_L7KcdH9IXszs")
+client.run("ODc5OTI3NzQ2Njg2NTA5MDg4.YSW2kw.jWL1lEK8iQCS6D-NgovkfFUV_ZA")
